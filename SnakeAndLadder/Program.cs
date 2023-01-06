c092385e0d4afc3f46ab227099a383d732e31930
Rutuja412 <rutujakadam4545@gmail.com>
2023-01-06 19:57:56 +0530

    [Rutuja]:Adding Start Position Of Game

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake And Ladder Program");
+            SnakeLadder snakeLadder=new SnakeLadder();
+            snakeLadder.Game();
+        }
+    }
+}
---
+++ SnakeAndLadder/SnakeLadder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLadder
+{
+    public class SnakeLadder
+    {
+        public void Game()
+        {
+            int position = 0;
+           Console.WriteLine("Single player at the start position " + position);
+        }
+    }
+}
